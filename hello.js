4ca526174637ee21e311c93bff65c6d55556cd32
dhamuprince <dhamupmtech@gmail.com>
2020-09-26 11:44:02 +0530

    hello file first change

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
 let name ="this string";
+let last = "last name here";
 console.log('this is first time i amusing', name)
-console.log('this is second line of code')
+console.log('this is second line of code', last)
